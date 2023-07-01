0a7bd6cd4a76deb15d999e63cfde130971c3de84
adogbeji <baka_1993@yahoo.co.uk>
2023-07-01 16:38:42 +0100

    Remove DEBUG banner & add icon to buyers home screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       ),
     );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,10 +13,14 @@ class HomeScreen extends StatelessWidget {
         right: 15,
       ),
       child: Row(
-        children: const <Widget>[
-          Text(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          const Text(
             'How are you? 👀',
             style: TextStyle(fontSize: 23),
+          ),
+          Container(
+            child: SvgPicture.asset('assets/icons/cart.svg', width: 20,)
           ),
         ],
       ),
