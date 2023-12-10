e8fff713aece92c4a3e84978422337d96cc54aed
adogbeji <baka_1993@yahoo.co.uk>
2023-12-10 18:08:20 +0000

    Import screens into buyers main screen & display home screen

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -2,6 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import './nav_screens/home_screen.dart';
+import './nav_screens/category_screen.dart';
+import './nav_screens/store_screen.dart';
+import './nav_screens/cart_screen.dart';
+import './nav_screens/account_screen.dart';
+
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -11,6 +17,14 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _pageIndex = 0;
+
+  List<Widget> _pages = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoreScreen(),
+    CartScreen(),
+    AccountScreen()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +63,7 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ],
       ),
+      body: _pages[0],
     );
   }
 }
