6edb5ddcb46ca66f141900bf7c41164c0dde2aa6
adogbeji <baka_1993@yahoo.co.uk>
2024-03-01 13:56:00 +0000

    Add TextField to buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -6,28 +6,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(
-            top: MediaQuery.of(context).padding.top,
-            left: 25,
-            right: 15,
-          ),
-          child: const Text(
-            'What are you looking for 👀?',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top,
+                left: 25,
+                right: 15,
+              ),
+              child: const Text(
+                'What are you looking for 👀?',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
+            Container(
+              child: SvgPicture.asset(
+                'assets/icons/cart.svg',
+                width: 20,
+              ),
+            ),
+          ],
         ),
-        Container(
-          child: SvgPicture.asset(
-            'assets/icons/cart.svg',
-            width: 20,
-          ),
-        ),
+
+        // SEARCH BAR
+        TextField(),
       ],
     );
   }
